98710eabcc5d9a935a7872acbaf081e9d4f5120b
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-02-03 16:56:57 +0300

    Added Function

---
+++ cipher.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+std::string crypter(const std::string text,int shift){
+    std::string result = "";
+    for (char i : text){
+        if( isupper(i) )
+        {
+            result += ( i - 'A' + shift ) % 26 + 'A' ;
+        } 
+        else if ( islower(i) )
+        {
+            result += ( i - 'a' + shift ) % 26 + 'a' ;
+        }
+        else
+        {
+            result += i ;
+        }
+    }
+    return result ;
+}
+
+int main(){
+    std::string text ;
+    int shift ;
+
+    std::cout << "Enter the text : " ;
+    std::cin >> text ;
+
+    std::cout << "Enter the amount of shift : ";
+    std::cin >> shift ;
+    
+    std::string result = crypter(text,shift);
+    std::cout << "crypted as : " << result ;
+
+    return 0 ;
+}
